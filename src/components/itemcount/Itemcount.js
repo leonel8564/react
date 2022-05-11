28e719ff54050612f10a090ab4b44d95894ce46b
leo dughetti <dughettileonel1@gmail.com>
2022-05-11 14:44:19 -0300

    boton2

--- src/components/itemcount/Itemcount.js
+++ src/components/itemcount/Itemcount.js
@@ -1,11 +1,42 @@
 import React from 'react'
 export default function Itemcount(){
     const [count, setCount] = React.useState(1)
+
+    let stock = 10
+
+    const sumar = () => {
+
+        if(count < stock){
+
+            setCount(count + 1 )
+
+        }
+
+    }
+
+
+
+    const restar = () => {
+
+        if(count > 0){
+
+            setCount(count -1)
+
+        }
+
+    }
+
     return(
+
         <div>
-        <input type="button" value="+" onClick={() => setCount(count + 1)} />
+
+        <input type="button" value="+" onClick={sumar} />
+
         {count}
-        <input type="button" value="-" onClick={() => setCount(count - 1)} />
+
+        <input type="button" value="-" onClick={restar} />
+
         </div>
+
     )
 }
