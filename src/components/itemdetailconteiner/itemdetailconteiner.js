cbe0aa1cdf0e21ae493604fc5b6d8fcc8bb548d9
leo dughetti <dughettileonel1@gmail.com>
2022-06-14 19:43:11 -0300

    anteultimo

--- src/components/itemdetailconteiner/itemdetailconteiner.js
+++ src/components/itemdetailconteiner/itemdetailconteiner.js
@@ -1,19 +1,39 @@
-
 import React,{useEffect,useState} from "react";
+
 import Itemdetail from "../itemdetail/itemdetail";
+
 import { doc, getDoc, getFirestore } from "firebase/firestore";
 
-const Itemdetailconteiner = ({id}) => {
+import { useParams } from "react-router-dom";
+
+
+
+const Itemdetailconteiner = () => {
+
     const [producto , setdetalleprod] = useState({})
+
+    const{id}=useParams()
+
     useEffect(()=>{
+
         const db= getFirestore()
+
         const productRef = doc(db,"productos",id)
+
             getDoc(productRef).then(snapshot =>{
+
                 setdetalleprod({id:snapshot.id, ...snapshot.data()})
+
             })
+
     },[id])
+
     return(
+
         <Itemdetail producto={producto}/>
+
     )
+
 }
+
 export default Itemdetailconteiner
--- src/components/itemlistcontainer/Itemlistcontainer.js
+++ src/components/itemlistcontainer/Itemlistcontainer.js
@@ -1,20 +1,15 @@
-
 import React, {useState } from "react";
-
 import Itemlist from "../itemlist/Itemlist";
-
 import { getFirestore , getDocs , collection, query, where,} from "firebase/firestore"
-
-
-export default function Itemlistcontainer({categoryid}) {
-
+import { useParams } from "react-router-dom";
+export default function Itemlistcontainer() {
     const[productos, setProductos]=useState([])
-
+    const{categoryid}= useParams()
     React.useEffect(()=>{
         const db = getFirestore()
         if(categoryid){
             const q = query(collection(db,"productos"),
-                where("category","==",categoryid)
+                where("category","==",Number(categoryid))
             );
             getDocs(q).then((snapshots) => {
                 if(snapshots.size === 0){
@@ -30,17 +25,26 @@ export default function Itemlistcontainer({categoryid}) {
         }
     },[categoryid])
 
-    
-
+    console.log(productos)
     return(
+
+
 
         <div>
 
+
+
             <Itemlist productos={productos}/>   
+
+
 
         </div>
 
+
+
     )
+
+
 
 }
 
